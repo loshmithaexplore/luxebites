e9177a9f70afa4030093732b5fcb37777f5606e5
Loshmitha <loshmithaxplore@gmail.com>
2026-09-17 16:37:57 +0530

    Fix live  allowed hosts

--- luxebites/wsgi.py
+++ luxebites/wsgi.py
@@ -7,15 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luxebites.settings')
-
-# application = get_wsgi_application()
-
-
 import os
 import sys
 
@@ -24,7 +15,7 @@ path = "/home/Loshmitha/luxe-bites"
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "luxebites.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luxebites.settings")
 
 from django.core.wsgi import get_wsgi_application
 
